b3f9f457e22540ba653d19eeb5c7d27a35b05478
Mur3ph <paulmurphymainstreet@gmail.com>
2019-05-20 22:53:07 +0100

    list and set data structures

--- src/main/java/ie/murph/java7/datastructures/AList.java
+++ src/main/java/ie/murph/java7/datastructures/AList.java
@@ -1,5 +1,36 @@
 package main.java.ie.murph.java7.datastructures;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
 public class AList {
+	
+	private List<String> list;
+	
+	public void createStringArrayList(){
+		list = new ArrayList<String>();
+	}
+	
+	public void createStringLinkedList(){
+		list = new LinkedList<String>();
+	}
+	
+	public void createStringVector(){
+		list =  new Vector<String>();
+	}
+	
+	public List<String> getStringArrayList(){
+		return new ArrayList<String>();
+	}
+	
+	public List<String> getStringLinkedList(){
+		return new LinkedList<String>();
+	}
+	
+	public List<String> getStringVector(){
+		return new Vector<String>();
+	}
 
 }
--- src/main/java/ie/murph/java7/datastructures/ASet.java
+++ src/main/java/ie/murph/java7/datastructures/ASet.java
@@ -1,5 +1,17 @@
 package main.java.ie.murph.java7.datastructures;
 
-public class ASet {
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
+public class ASet { //implements SortedSet<Object>{
+	
+	private static Set set;
+
+	public static void main(){
+		set = new HashSet();
+		set = new LinkedHashSet();
+		set = new TreeSet();
+	}
 }
