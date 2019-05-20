0ced9c8db94292c07370a962fec27cef9f8a8b34
Mur3ph <paulmurphymainstreet@gmail.com>
2019-05-20 22:59:40 +0100

    map and q data structures

--- src/main/java/ie/murph/java7/datastructures/AMap.java
+++ src/main/java/ie/murph/java7/datastructures/AMap.java
@@ -1,5 +1,19 @@
 package main.java.ie.murph.java7.datastructures;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AMap {
+
+	private static Map map;
+	private static Hashtable hashTable;
+
+	public static void main(){
+		map = new HashMap();
+		map = new TreeMap();
+		hashTable = new Hashtable();
+	}
 
 }
--- src/main/java/ie/murph/java7/datastructures/AQueue.java
+++ src/main/java/ie/murph/java7/datastructures/AQueue.java
@@ -1,5 +1,13 @@
 package main.java.ie.murph.java7.datastructures;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class AQueue {
 
+	private static Queue q;
+
+	public static void main(){
+		q = new PriorityQueue();
+	}
 }
