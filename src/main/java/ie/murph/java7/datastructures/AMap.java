139e91e9a1376ad2d79f6cdc8610778b6cb7cde1
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-02 22:11:27 +0100

    data structures hash map table and code

--- src/main/java/ie/murph/java7/datastructures/AMap.java
+++ src/main/java/ie/murph/java7/datastructures/AMap.java
@@ -7,13 +7,36 @@ import java.util.TreeMap;
 
 public class AMap {
 
-	private static Map map;
-	private static Hashtable hashTable;
+	private static Map<Character, String> map;
+	private static Hashtable<Character, String> hashTable;
 
 	public static void main(String[] args){
-		map = new HashMap();
-		map = new TreeMap();
-		hashTable = new Hashtable();
+		AMap aMap = new AMap();
+		
+		map = aMap.createHashMap();
+		map.put('A', "Apple");
+		System.out.println(map.hashCode());
+		
+		map = aMap.createTreeMap();
+		map.clear();
+		map.put('B', "Ball");
+		System.out.println(map.hashCode());
+		
+		hashTable = aMap.createHashtable();
+		hashTable.put('P', "Purple");
+		System.out.println(hashTable.hashCode());
+	}
+	
+	private HashMap<Character, String> createHashMap(){
+		return new HashMap<Character, String>();
+	}
+	
+	private TreeMap<Character, String> createTreeMap(){
+		return new TreeMap<Character, String>();
+	}
+	
+	private Hashtable<Character, String> createHashtable(){
+		return new Hashtable<Character, String>();
 	}
 
 }
