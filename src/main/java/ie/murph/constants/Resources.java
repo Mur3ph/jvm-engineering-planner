ad22582f88b109b6c8350221cccf3096fd80f298
Mur3ph <paulmurphymainstreet@gmail.com>
2019-12-09 23:28:52 +0000

    resources

--- src/main/java/ie/murph/constants/Resources.java
+++ src/main/java/ie/murph/constants/Resources.java
@@ -2,8 +2,9 @@ package main.java.ie.murph.constants;
 
 public enum Resources
 {
-	CONNECTION_LOST("Connection to server lost: "),
-	_THE_BEST_OF_JAVA_COLLECTIONS_TUTORIALS("https://dzone.com/articles/the-best-of-java-collections-tutorials?utm_source=dzone&utm_medium=article&utm_campaign=java-streams-cluster");
+	_AN_INTRODUCTION_TO_THE_JAVA_COLLECTIONS_FRAMEWORK("https://dzone.com/articles/an-introduction-to-the-java-collections-framework?utm_source=dzone&utm_medium=article&utm_campaign=java-collections-cluster"),
+	_THE_BEST_OF_JAVA_COLLECTIONS_TUTORIALS("https://dzone.com/articles/the-best-of-java-collections-tutorials?utm_source=dzone&utm_medium=article&utm_campaign=java-streams-cluster"),
+	_A_DEVELOPERS_GUIDE_TO_COLLECTIONS("https://dzone.com/articles/a-deep-dive-into-collections?utm_source=dzone&utm_medium=article&utm_campaign=java-collections-cluster");
 	
 	private final String text;
 
