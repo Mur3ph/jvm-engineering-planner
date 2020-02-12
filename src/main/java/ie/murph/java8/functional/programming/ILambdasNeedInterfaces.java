6b2635a96e62ab1f91d71634f7ec7705cfbc38ea
Mur3ph <paulmurphymainstreet@gmail.com>
2020-02-12 22:40:21 +0000

    lambdas practice

---
+++ src/main/java/ie/murph/java8/functional/programming/ILambdasNeedInterfaces.java
@@ -0,0 +1,5 @@
+package main.java.ie.murph.java8.functional.programming;
+
+public interface ILambdasNeedInterfaces {
+	boolean ApplyArg(int x);
+}
--- src/main/java/ie/murph/java8/functional/programming/Lambdas.java
+++ src/main/java/ie/murph/java8/functional/programming/Lambdas.java
@@ -2,4 +2,34 @@ package main.java.ie.murph.java8.functional.programming;
 
 public class Lambdas {
 
+	public static void main(String[] args) {
+		Lambdas lambdas = new Lambdas();
+		
+		lambdas.internalInterfaceTest();
+		lambdas.threadInterfaceTest();
+	}
+	
+	private void internalInterfaceTest(){
+		System.out.println("=== Internal Interface Test ===");
+		ILambdasNeedInterfaces isEven = (n) -> (n % 2) == 0;
+		System.out.println(isEven.ApplyArg(4));
+	}
+	
+	private void threadInterfaceTest(){
+		System.out.println("=== RunnableTest ===");
+		// Anonymous Runnable
+		Runnable r1 = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Hello world Anonymous!");
+			}
+		};
+
+		// Lambda Runnable
+		Runnable r2 = () -> System.out.println("Hello world Lambda!");
+		// Run em!
+		r1.run();
+		r2.run();
+	}
+
 }
