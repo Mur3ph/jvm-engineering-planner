6093be2643b4652cf65dd26404efa616a2d739ca
Mur3ph <paulmurphymainstreet@gmail.com>
2020-03-06 22:37:31 +0000

    streams

--- src/main/java/ie/murph/java8/functional/programming/Streams_FilterMapReduce.java
+++ src/main/java/ie/murph/java8/functional/programming/Streams_FilterMapReduce.java
@@ -1,5 +1,25 @@
 package main.java.ie.murph.java8.functional.programming;
 
+import java.util.stream.Stream;
+
 public class Streams_FilterMapReduce {
+	
+	public static void main(String[] args){
+		Streams_FilterMapReduce streams = new Streams_FilterMapReduce();
+		streams.streamBuilderPattern1();
+	}
+
+	public void streamBuilderPattern1() {
+		Stream<String> streamBuilder = Stream.<String>builder().add("a").add("b").add("c").build();
+		// Print the stream 
+		streamBuilder.forEach(s -> System.out.println(s)); 
+	}
+	
+	public void streamBuilderPattern2() {
+		Stream<String> streamBuilder = Stream.<String>builder().add("a").add("b").add("c").build();
+		 // Print the stream 
+		streamBuilder.forEach(System.out::println); 
+	}
+	
 
 }
